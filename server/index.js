a4ebfd98d9cf07e90e9289593a7be685c7641d06
Cody <relationalprogrammer@gmail.com>
2021-11-17 13:42:16 -0600

    should be production ready

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const path = require("path");
+
+app.use('/js', express.static(path.join(__dirname, '../main.js')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4004
+
+app.listen(port, () => console.log(`Running on port ${port}`));
