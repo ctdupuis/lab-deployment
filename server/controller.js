ad0f5340bd1cccafc11584a5c9f86338daedd99a
Cody <relationalprogrammer@gmail.com>
2021-11-17 14:05:27 -0600

    changed file path for index.html

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 module.exports = {
-    home: (req, res) => res.sendFile(path.join(__dirname, '../client/index.html'))
+    home: (req, res) => res.sendFile(path.join(__dirname, '../src/index.html'))
 };
