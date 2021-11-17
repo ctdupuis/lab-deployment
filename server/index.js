74df81c0e8e08c74926534b3b72dbb15f49de5b8
Cody <relationalprogrammer@gmail.com>
2021-11-17 13:52:21 -0600

    fixed path to index.html

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 // app.use('/js', express.static(path.join(__dirname, '../main.js')));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 });
 
 const port = process.env.PORT || 4004
