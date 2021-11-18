a318ea3327c16345a3fcd523aa7ebd6f258c6485
Cody <relationalprogrammer@gmail.com>
2021-11-18 15:33:47 -0600

    added some html, css, rollbar and sequelize

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,18 @@
 const path = require('path');
 
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+
 var Rollbar = require('rollbar');
 
 var rollbar = new Rollbar({
@@ -9,6 +22,10 @@ var rollbar = new Rollbar({
 });
 
 module.exports = {
-    home: (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')),
-    about: (req, res) => res.sendFile(path.join(__dirname, '../public/about.html'))
+    home: (req, res) => {
+        rollbar.info("Root endpoint hit");
+        console.log(ROLLBAR_TOKEN, DATABASE_URL)
+        console.log("HI I;M DOING A THING")
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    }
 };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-require('dotenv').config();
-
 const app = express();
 
 const path = require("path");
@@ -9,16 +7,12 @@ const path = require("path");
 app.use(express.static("public"));
 
 
-
-
 const {
-    home,
-    about
+    home
 } = require('./controller');
 
 app.get("/", home);
-app.get("/", about);
 
-const port = process.env.PORT || 4004
+const port = process.env.PORT || 4105
 
 app.listen(port, () => console.log(`Running on port ${port}`));
